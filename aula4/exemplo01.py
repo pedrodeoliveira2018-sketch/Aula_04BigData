94ea08de7811a4a83882e403a95a52a37115a5ab
PedroDeOliveira <pedrodeoliveira2018@icloud.com>
2025-11-25 20:36:42 -0300

    aula de if else

---
+++ aula4/exemplo01.py
@@ -0,0 +1,23 @@
+# # Verificação de mair idade !
+# idade =  int (input("Informe sua Idade: "))
+
+# if idade >= 18: 
+#     print('você é maior de idade')
+# else:
+#     print('você menor de idade ')
+
+
+# testatndo mais condições 
+# teste de pontos
+
+# pontos = int (input('quantos pontos ?'))
+
+# if pontos >= 100:
+#     print('nivel maximo!')
+# elif pontos >= 50:
+#     print('nivel bom !')
+# elif pontos >= 25:
+#     print('Pontuação ruim!')
+
+# else:
+#     print('você precisa melhorar')
