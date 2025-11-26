8ebdb94b8b5793247d54908856f771e07b8b76fc
PedroDeOliveira <pedrodeoliveira2018@icloud.com>
2025-11-25 21:28:01 -0300

    novas questões

--- aula4/exemplo01.py
+++ aula4/exemplo01.py
@@ -21,3 +21,35 @@
 
 # else:
 #     print('você precisa melhorar')
+
+# verificação de login 
+
+# usuario = input ('informe o usuario ')
+# senha = input('informe a senha ! ')
+
+# if usuario == ' adimin' and senha == ' 1234':
+#     print('O Pai Ta On ')
+
+# else:
+#     print('Usuario invalido!')
+    
+# Condição para bride 
+
+# compra = float(input('valor da compra '))
+# cliente10 = input( 'cliente 10 ? [S/N]').lower ()
+
+# if compra >= 1000 or cliente10 == 's':
+#     print('voce tem direito a brinde')
+# else:
+#     print ('talvez na proxima!')
+
+nota = float(input('informe a nota '))
+frequencia = float(input('informe a frequencia'))
+
+if nota >= 7 :
+    if frequencia >= 75 :
+        print('Aprovado')
+    else :
+        print('boa nota , mas reprovado por falta')
+else :
+    print('reprovado')        
